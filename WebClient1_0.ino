a6128579fcda08cd0812e3ff89183d606edff056
quaint <twisnia@gmail.com>
2012-01-07 15:31:48 +0100

    cleanups

--- WebClient1_0.ino
+++ WebClient1_0.ino
@@ -4,13 +4,16 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 
-#define OUT_PIN_1 7
+#define OUT_PIN_1 0
 #define OUT_PIN_2 1
-#define ANALOG_PIN_1 0
+#define TEMP_ANALOG_PIN 0
+#define EXPANDER_ADDR 0x20
+#define LCD_TEMPLATE "X X X X X X X X"
+#define SERVER_PORT 3000
 
+String sensorSerialNumber = "12345";
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x41, 0xF0 };
 //char serverName[] = "gentle-planet-1993.heroku.com";
-//char serverName[] = "quaint.ovh.org";
 char serverName[] = "10.0.1.103";
 
 EthernetClient client;
@@ -19,19 +22,22 @@ LiquidCrystal lcd(8, 9, 6, 5, 3, 2);
 
 void setup() {
   Serial.begin(9600);
-  expander.begin(0x20);
-  pinMode(OUT_PIN_1, OUTPUT);
+  
+  expander.begin(EXPANDER_ADDR);
+  expander.pinMode(OUT_PIN_1, OUTPUT);
   expander.pinMode(OUT_PIN_2, OUTPUT);
-//  expander.digitalWrite(OUT_PIN_1, HIGH);
+  expander.digitalWrite(OUT_PIN_1, HIGH);
   expander.digitalWrite(OUT_PIN_2, HIGH);
+  
   lcd.begin(16, 2);
   lcd.setCursor(0, 1);
-  lcd.print("X X X X X X X X");
-  if (Ethernet.begin(mac) == 0) {
+  lcd.print(LCD_TEMPLATE);
+  
+	if (Ethernet.begin(mac) == 0) {
     Serial.println("failed to configure Ethernet using DHCP");
     while(true);
   }
-  /*
+
   Serial.print("IP address: ");
   for (byte thisByte = 0; thisByte < 4; thisByte++) {
     // print the value of each byte of the IP address:
@@ -39,30 +45,43 @@ void setup() {
     Serial.print(".");
   }
   Serial.println();
-  */
+
   delay(1000);
   
-  Serial.println("connecting...");
+  Serial.println("starting...");
 }
 
 void loop()
 {
-  if (client.connect(serverName, 3000) > 0) {
-    lcd.setCursor(0, 0);
-    lcd.print("syncing ");
-    Serial.println("connected");
-    client.println("GET /boards/sync/12345.xml HTTP/1.0");
-//    client.println("GET /output.php HTTP/1.0");
-//    client.println("Host: quaint.ovh.org");
-    client.println();
-  } else {
-    Serial.println("connection failed");
-  }
+	handleSync();
+	handleTemp();
+	handleCountdown();
+}
 
-  if (client.connected()) {
-    Serial.println("parser start");
+void handleCountdown() {
+	lcd.setCursor(0, 0);
+  lcd.print("t-        ");
+  for(int i=10; i>0; i--) {
+    lcd.setCursor(2, 0);
+    lcd.print(i);
+    if(i<10) {
+      lcd.setCursor(3, 0);
+      lcd.print(" ");
+    }
+    delay(1000);
+  }
+}
+
+void handleSync() {
+	if (client.connect(serverName, SERVER_PORT) > 0) {
+    Serial.println("connected");
+		lcd.setCursor(0, 0);
+    lcd.print("syncing ");
+    client.println("GET /boards/sync/12345.xml HTTP/1.0");
+    client.println("Host: " + String(serverName));
+    client.println();
+
     while( client.find("<port>") ) {
-      Serial.println("port found");
       int port = client.parseInt();
       Serial.print("port: ");
       Serial.println(port);
@@ -76,10 +95,10 @@ void loop()
         if(port == 1) {
           lcd.setCursor(0, 1);
           if(state == 1) {
-            digitalWrite(OUT_PIN_1, HIGH);
+            expander.digitalWrite(OUT_PIN_1, LOW);
             lcd.print("1");
           } else if(state == 0) {
-            digitalWrite(OUT_PIN_1, LOW);
+            expander.digitalWrite(OUT_PIN_1, HIGH);
             lcd.print("0");
           }
         } else if(port == 2) {
@@ -96,22 +115,28 @@ void loop()
     }
     client.stop();
     client.flush();
-    
-    float temp = readTemp();
+  } else {
+	  Serial.println("not connected when syncing"); 
+	}
+}
+
+void handleTemp() {
+	  float temp = readTemp();
     lcd.setCursor(10, 0);
     lcd.print(temp);
     Serial.print("temp: ");
     Serial.println(temp);
     
     String parValue = "value=";
-    String parSerial = "&serial=12345";
+    String parSerial = "&serial=" + sensorSerialNumber;
     
     Serial.println("sending temp");
     lcd.setCursor(0, 0);
     lcd.print("sending   ");
-    if (client.connect(serverName,3000)>0) {
+    if (client.connect(serverName, SERVER_PORT) > 0) {
       client.println("POST /readings/upload HTTP/1.0");
       client.println("Content-Type: application/x-www-form-urlencoded");
+			client.println("Host: " + String(serverName));
       client.print("Content-Length: ");
       client.println(parValue.length() + parSerial.length() + sizeof(temp) + 1);
       client.println();
@@ -125,26 +150,11 @@ void loop()
       lcd.setCursor(0, 0);
       lcd.print("sent      ");
     } else {
-      Serial.println("disconnected when sending"); 
+      Serial.println("not connected when sending"); 
     }
-  } else {
-    Serial.println("disconnected"); 
-  }
-  
-  lcd.setCursor(0, 0);
-  lcd.print("t-        ");
-  for(int i=10; i>0; i--) {
-    lcd.setCursor(2, 0);
-    lcd.print(i);
-    if(i<10) {
-      lcd.setCursor(3, 0);
-      lcd.print(" ");
-    }
-    delay(1000);
-  }
 }
 
 float readTemp() {
-  float temp = analogRead(ANALOG_PIN_1) * 5/1024.0;
+  float temp = analogRead(TEMP_ANALOG_PIN) * 5/1024.0;
   return (temp - 0.5) / 0.01;
 }
