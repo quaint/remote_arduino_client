f7a4cd90b116202a29edec14352bc7f0d55ea7fc
quaint <twisnia@gmail.com>
2012-01-09 22:18:02 +0100

    added light sensor

--- WebClient1_0.ino
+++ WebClient1_0.ino
@@ -3,30 +3,39 @@
 #include <PCF8574.h>
 #include <Wire.h>
 #include <LiquidCrystal.h>
+#include <Servo.h>
 
 #define OUT_PIN_1 0
 #define OUT_PIN_2 1
 #define TEMP_ANALOG_PIN 0
+#define LIGHT_ANALOG_PIN 1
 #define EXPANDER_ADDR 0x20
 #define LCD_TEMPLATE "X X X X X X X X"
-//#define SERVER_PORT 3000
-#define SERVER_PORT 80
+#define SERVER_PORT 3000
+//#define SERVER_PORT 80
 #define SYNC_DEALY 10
-#define TEMP_RATIO 360
 
-String sensorSerialNumber = "12345";
+
+String tempSensorSN = "12345";
+String lightSensorSN = "11111";
 String boardSerialNumber = "12345";
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x41, 0xF0 };
-char serverName[] = "gentle-planet-1993.heroku.com";
-//char serverName[] = "10.0.1.101";
-int tempRatioCounter = 0;
+//char serverName[] = "gentle-planet-1993.heroku.com";
+char serverName[] = "10.0.1.101";
+int tempSensorRatioCounter = 0;
+int tempSensorRatio = 10;
+int lightSensorRatioCounter = 0;
+int lightSensorRatio = 10;
 
 EthernetClient client;
 PCF8574 expander;
-LiquidCrystal lcd(8, 9, 6, 5, 3, 2);
+LiquidCrystal lcd(8, 9, 7, 5, 3, 2);
+Servo servo;
 
 void setup() {
   Serial.begin(9600);
+  
+  servo.attach(6);
   
   expander.begin(EXPANDER_ADDR);
   expander.pinMode(OUT_PIN_1, OUTPUT);
@@ -60,12 +69,13 @@ void loop()
 {
   handleSync();
   handleTemp();
+  handleLight();
   handleCountdown();
 }
 
 void handleCountdown() {
   lcd.setCursor(0, 0);
-  lcd.print("t-        ");
+  lcd.print("t-      ");
   for(int i=SYNC_DEALY; i>0; i--) {
     lcd.setCursor(2, 0);
     lcd.print(i);
@@ -131,40 +141,62 @@ void handleTemp() {
   lcd.print(temp);
   Serial.print("temp: ");
   Serial.println(temp);
-  
-  if(tempRatioCounter >= TEMP_RATIO) {
-    tempRatioCounter = 0;
-    String parValue = "value=";
-    String parSerial = "&serial=" + sensorSerialNumber;
-      
-    Serial.println("sending temp");
-    lcd.setCursor(0, 0);
-    lcd.print("sending   ");
-    if (client.connect(serverName, SERVER_PORT) > 0) {
-      client.println("POST /readings/upload HTTP/1.0");
-      client.println("Content-Type: application/x-www-form-urlencoded");
-      client.println("Host: " + String(serverName));
-      client.print("Content-Length: ");
-      client.println(parValue.length() + parSerial.length() + sizeof(temp) + 1);
-      client.println();
-      client.print(parValue);
-      client.print(temp);
-      client.println(parSerial);
-      client.println();
-      client.stop();
-      client.flush();
-      Serial.println("temp sent");
-      lcd.setCursor(0, 0);
-      lcd.print("sent      ");
-    } else {
-      Serial.println("not connected when sending"); 
-    }
+  if(tempSensorRatioCounter >= tempSensorRatio) {
+    tempSensorRatioCounter = 0;
+    handleSensor(tempSensorSN, temp);
   } else {
-    tempRatioCounter++;
+    tempSensorRatioCounter++;
+  }
+}
+
+void handleLight() {
+  int light = readLight();
+  lcd.setCursor(8, 0);
+  lcd.print(light);
+  Serial.print("light: ");
+  Serial.println(light);
+  if(lightSensorRatioCounter >= lightSensorRatio) {
+    lightSensorRatioCounter = 0;
+    handleSensor(lightSensorSN, (float)light);
+  } else {
+    lightSensorRatioCounter++;
+  }
+}
+
+void handleSensor(String serial, float reading) {
+  
+  String parSerial = "serial=" + serial;
+  String parValue = "&value=";
+      
+  Serial.println("sending data");
+  lcd.setCursor(0, 0);
+  lcd.print("sending   ");
+  if (client.connect(serverName, SERVER_PORT) > 0) {
+    client.println("POST /readings/upload HTTP/1.0");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Host: " + String(serverName));
+    client.print("Content-Length: ");
+    client.println(parValue.length() + parSerial.length() + Serial.println(reading));
+    client.println();
+    client.print(parSerial);
+    client.print(parValue);
+    client.println(reading);
+    client.println();
+    client.stop();
+    client.flush();
+    Serial.println("data sent");
+    lcd.setCursor(0, 0);
+    lcd.print("sent      ");
+  } else {
+    Serial.println("not connected when sending"); 
   }
 }
 
 float readTemp() {
   float temp = analogRead(TEMP_ANALOG_PIN) * 5/1024.0;
   return (temp - 0.5) / 0.01;
+}
+
+int readLight() {
+  return analogRead(LIGHT_ANALOG_PIN) / 128;
 }
